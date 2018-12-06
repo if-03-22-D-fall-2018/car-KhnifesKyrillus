8c8877e11ab228222fe0fa3509596bb2a4cfa949
Khnifes Kyrillus <khnifes.kyrillus@gmail.com>
2018-12-06 09:42:50 +0100

    created all cars

--- car.cpp
+++ car.cpp
@@ -11,12 +11,26 @@ struct ImplementationCar
   bool is_available;
 };
 
-static Car car_park[20];
+struct ImplementationCar aixam { AIXAM, RED, 16.0, 0.0, 0, true };
+struct ImplementationCar fiat_mutipla1 { FIAT_MULTIPLA, GREEN, 65.0, 0.0, 0, true };
+struct ImplementationCar fiat_mutipla2 { FIAT_MULTIPLA, BLUE, 65.0, 0.0, 0, true };
+struct ImplementationCar fiat_mutipla3 { FIAT_MULTIPLA, ORANGE, 65.0, 0.0, 0, true };
+struct ImplementationCar jeep1 { JEEP, SILVER, 80.0, 0.0, 0, true };
+struct ImplementationCar jeep2 { JEEP, BLACK, 80.0, 0.0, 0, true };
+
+
+static Car car_park[20] = {&aixam};
 
 void init()
 {
-
+  
 }
+
+Car get_car(enum CarType carType)
+{
+  return &aixam;
+}
+
 enum CarType get_type(Car car)
 {
   return car->carType;
@@ -38,10 +52,6 @@ void set_acceleration_rate(Car car, double acceleration)
 
 }
 int get_speed(Car car)
-{
-  return 0;
-}
-Car get_car(enum CarType carType)
 {
   return 0;
 }
