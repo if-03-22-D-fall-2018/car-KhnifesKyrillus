0364900890ac6cec24266bb742fded41ffe4309b
Khnifes Kyrillus <khnifes.kyrillus@gmail.com>
2018-12-06 09:14:27 +0100

    car.h finished

---
+++ car.cpp
@@ -0,0 +1,49 @@
+#include "car.h"
+
+struct ImplementationCar
+{
+  enum CarType carType;
+  enum Color corlor;
+  double fill_level;
+  double acceleration_rate;
+  int speed;
+};
+
+static Car car_park[20];
+
+void init()
+{
+
+}
+enum CarType get_type(Car car)
+{
+  return car->carType;
+}
+enum Color get_color(Car car)
+{
+  return car->color;
+}
+double get_fill_level(Car car)
+{
+  return 1111111.002;
+}
+double get_acceleration_rate(Car car)
+{
+  return 1111111.002;
+}
+void set_acceleration_rate(Car car, double acceleration)
+{
+
+}
+int get_speed(Car car)
+{
+  return 0;
+}
+Car get_car(enum CarType carType)
+{
+  return 0;
+}
+void accelerate(Car car)
+{
+
+}
--- car.h
+++ car.h
@@ -13,4 +13,26 @@
 #ifndef ___CAR_H
 #define ___CAR_H
 
+enum CarType
+{
+  JEEP,FIAT_MULTIPLA,AIXAM
+};
+
+enum Color
+{
+  SILVER,BLUE,ORANGE,BLACK,RED,GREEN
+};
+
+typedef struct ImplementationCar* Car;
+
+void init();
+enum CarType get_type(Car car);
+enum Color get_color(Car car);
+double get_fill_level(Car car);
+double get_acceleration_rate(Car car);
+void set_acceleration_rate(Car car, double acceleration);
+int get_speed(Car car);
+Car get_car(enum CarType carType);
+void accelerate(Car car);
+
 #endif
